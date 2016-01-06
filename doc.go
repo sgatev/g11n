a50dd006f9dbd7587722591549c47491eb938d82
Stanislav Gatev <mail@s2gatev.com>
2016-01-06 11:00:36 +0200

    Add doc file for g11n

---
+++ doc.go
@@ -0,0 +1,37 @@
+// g11n is an internationalization library that offers:
+//
+//	* Statically-typed message keys.
+//	* Parameterized messages.
+//	* Extendable message formatting.
+//	* Custom localization file format.
+//
+//
+// I. Initialization
+//
+// Create a new instance of g11n. Each instance handles messages and locales separately.
+//
+//	G = g11n.New()
+//
+// Define a struct with messages.
+//
+//	type Messages struct {
+//		TheAnswer func(string, int) string `default:"The answer to %v is %v."`
+//	}
+//
+// Initialize an instance of the struct through the g11n object.
+//
+//	var M *Messages
+//	G.Init(M)
+//
+// Invoke messages on that instance.
+//
+//	M.TheAnswer("everything", 42")
+//
+//
+// II. Choosing locale
+//
+// Specify the locale for every message struct initialized by this g11n instance.
+//
+//	G.SetLocale("en")
+//
+package g11n
