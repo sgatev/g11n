cb4e63d9076d35c26188afa6f01e4eea657483cd
Stanislav Gatev <mail@s2gatev.com>
2016-02-20 01:28:57 +0200

    Add doc in locale package

---
+++ locale/doc.go
@@ -0,0 +1,23 @@
+// Package locale presents localization loaders for the g11n internationalization library.
+//
+//
+// I. Creating a locale loader
+//
+// Every locale loader (built-in or custom) should implement the Loader interface and register itself in the loaders registry under some specific format name using RegisterLoader.
+//
+//	func init() {
+//		RegisterLoader("custom", customLoader{})
+//	}
+//
+//
+// II. Retrieving a locale loader
+//
+// A locale loader could be retrieved from the loaders registry using GetLoader.
+//
+//	loader, ok := GetLoader("custom") (Loader, bool)
+//
+//
+// III. Built-in locale loaders
+//
+// *g11n* comes with two built-in locale loaders - *"json"* and *"yaml"*.
+package locale
